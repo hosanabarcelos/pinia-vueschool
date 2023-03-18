4f8ab77cb92f8960766f79f2265ce0132553404f
Hosana <hosanabarceloscandido@gmail.com>
2023-03-18 16:01:25 -0300

    feat: add store de produtos

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import products from "@/data/products.json";
+
+import { useProductStore } from "@/stores/ProductStore";
+
+const productStore = useProductStore();
+productStore.fill();
 </script>
 
 <template>
@@ -9,7 +13,7 @@ import products from "@/data/products.json";
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
-        v-for="product in products"
+        v-for="product in productStore.products"
         :key="product.name"
         :product="product"
       />
---
+++ src/stores/ProductStore.js
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia";
+
+export const useProductStore = defineStore("ProductStore", {
+    //state
+    state: () => {
+        return {
+            products: [],
+        };
+    },
+
+    //actions
+    actions: {
+        async fill() {
+            this.products = (await import ("@/data/products.json")).default;
+        }
+    }
+
+    //getters
+});
